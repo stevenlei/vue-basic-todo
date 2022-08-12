33ec459d405a6d23ea8cae26adadcf1ec7b88ca1
Steven Lei <stevenplus@gmail.com>
2022-08-12 15:32:27 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  
+};
+</script>
+
+<template>
+  <div class="min-h-screen w-full bg-slate-100 flex justify-center items-center">
+    <div class="bg-white flex-1 max-w-md rounded-xl shadow-xl overflow-hidden">
+      <input type="text" class="bg-slate-600 text-white p-6 w-full outline-none text-3xl" placeholder="Type something...">
+      <ul>
+        <li class="p-6 text-3xl transition border-b border-slate-100 hover:bg-blue-500 hover:text-white cursor-pointer bg-slate-200 text-slate-600">
+          Buy something
+        </li>
+        <li class="p-6 text-3xl transition border-b border-slate-100 hover:bg-blue-500 hover:text-white cursor-pointer bg-slate-200 text-slate-600">
+          Buy something
+        </li>
+        <li class="p-6 text-3xl transition border-b border-slate-100 hover:bg-blue-500 hover:text-white cursor-pointer bg-slate-200 text-slate-600">
+          Buy something
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
